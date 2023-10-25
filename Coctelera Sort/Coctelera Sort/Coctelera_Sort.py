d00b58386652fdaac5e4a6f439bd07309f6454ca
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-10-24 21:10:59 -0600

    Coctelera Sort

---
+++ Coctelera Sort/Coctelera Sort/Coctelera_Sort.py
@@ -0,0 +1,11 @@
+
+def cocktail_shaker_sort(arr):
+        start = start + 1
+
+# Ejemplo de uso
+arr = [64, 54, 25, 12, 22, 21, 68]
+cocktail_shaker_sort(arr)
+
+print("Lista ordenada:")
+for element in arr:
+    print(element)

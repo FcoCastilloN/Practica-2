3cdb59dd28c97848b5ea5845d48aa4a4f7281214
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-10-24 21:57:09 -0600

    Clasificación de burbujas optimizada

---
+++ Clasificación de burbujas optimizada/Clasificación de burbujas optimizada/Clasificación_de_burbujas_optimizada.py
@@ -0,0 +1,11 @@
+
+def optimized_bubble_sort(arr):
+                swapped = True  # Se han realizado intercambios en esta pasada
+
+# Ejemplo de uso
+arr = [64, 34, 25, 12, 22, 11, 90]
+optimized_bubble_sort(arr)
+
+print("Lista ordenada:")
+for element in arr:
+    print(element)

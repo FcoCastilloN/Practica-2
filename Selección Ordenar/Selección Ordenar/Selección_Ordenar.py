208e0714ded6853b3a532d992c44c524d7609101
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-10-24 22:21:27 -0600

    Selección Ordenar

---
+++ Selección Ordenar/Selección Ordenar/Selección_Ordenar.py
@@ -0,0 +1,19 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Encuentra el elemento mínimo en la parte no ordenada
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        
+        # Intercambia el elemento mínimo con el elemento en la posición actual
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+# Ejemplo de uso
+arr = [64, 25, 12, 22, 11]
+selection_sort(arr)
+
+print("Lista ordenada:")
+for i in range(len(arr)):
+    print(arr[i])

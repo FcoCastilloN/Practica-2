b09cad13ae488e95f520cb39669abbc9c72df7a1
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-10-24 22:04:33 -0600

    Ordenar cubo

---
+++ Ordenar cubo/Ordenar cubo/Ordenar_cubo.py
@@ -0,0 +1,10 @@
+def bucket_sort(arr):
+    return sorted_buckets
+
+# Ejemplo de uso
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = bucket_sort(arr)
+
+print("Lista ordenada:")
+for element in sorted_arr:
+    print(element)
